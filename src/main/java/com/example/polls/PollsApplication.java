5ff766add997ee3e4a540299167f236e5393a06c
jlvillanueva <jorge.villanueva@atomic32.com>
2020-05-30 12:28:06 -0500

    Setting server app
    - Adding additional dependencies
    - Configuring server, hibernate and Jackson
    - Setting the application default timezone to UTC.

--- src/main/java/com/example/polls/PollsApplication.java
+++ src/main/java/com/example/polls/PollsApplication.java
@@ -1,10 +1,19 @@
 package com.example.polls;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PollsApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollsApplication.class, args);
